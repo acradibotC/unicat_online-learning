65291ba010f000aef601c62471ebe796eafeed4f
manhndhe163515 <manhdinh310@gmail.com>
2023-03-21 15:46:34 +0700

    update

--- unicat_online_learning/src/main/java/com/unicat/onlinelearning/controller/course/Course_Manager_Insert_Transaction.java
+++ unicat_online_learning/src/main/java/com/unicat/onlinelearning/controller/course/Course_Manager_Insert_Transaction.java
@@ -30,7 +30,7 @@ public class Course_Manager_Insert_Transaction extends HttpServlet {
             c.setCourseInfo(req.getParameter("txtCourseInf"));
             c.setDescription(req.getParameter("txtCourseDescription"));
             c.setPublishStatus(0);
-            c.setRequest("Updating");
+            c.setRequest("None");
             cd.AddnewCourse(c);
             com.unicat.onlinelearning.dto.Course course = cd.getLastCourseCreatedByUserID(user.getUserID());
             resp.sendRedirect(req.getContextPath() + "/tutor/manager/course?page=view&CourseID=" + course.getCourseID());
